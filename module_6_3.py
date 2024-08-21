bad00e16871137c8317b3dc6e59bc3ce5ae0d318
Sergey Vanyukhin <58528567+serg1570@users.noreply.github.com>
2024-08-21 20:54:29 +0300

    Множественное наследование
    Домашнее задание по теме "Множественное наследование"

---
+++ module_6_3.py
@@ -0,0 +1,51 @@
+class Horse:
+
+    def __init__(self):
+        self.x_distance = 0  # - пройденный путь.
+        self.sound = 'Frrr'  # - звук, который издаёт лошадь.
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+
+    def __init__(self):
+        self.y_distance = 0  # - высота полёта
+        self.sound = 'I train, eat, sleep, and repeat' # - звук, который издаёт орёл (отсылка)
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        super().__init__()
+        Eagle.__init__(self)
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+def main():
+    p1 = Pegasus()
+
+    print(p1.get_pos())
+    p1.move(10, 15)
+    print(p1.get_pos())
+    p1.move(-5, 20)
+    print(p1.get_pos())
+
+    p1.voice()
+
+
+if __name__ == '__main__':
+    main()
